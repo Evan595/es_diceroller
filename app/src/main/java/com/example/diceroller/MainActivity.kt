981f44456aa335d94f0aa564b21ad056b5a06ae8
Evan595 <53194327+Evan595@users.noreply.github.com>
2019-07-29 20:19:59 -0500

    NewDiceRoller

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -12,7 +12,7 @@ import kotlin.random.Random
 
 
 class MainActivity : AppCompatActivity() {
-
+    lateinit var diceImageView: ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -20,13 +20,15 @@ class MainActivity : AppCompatActivity() {
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.setOnClickListener {
             Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
-            rollDice()
+            rollonethousand()
+
         }
     }
 
-    private fun rollDice() {
-        val randomInt = Random.nextInt(6) + 1
-        val drawableResource = when (randomInt){
+    private fun rollDice(): Boolean {
+        val dice1 = Random.nextInt(6) + 1
+        val dice2 = Random.nextInt(6) + 1
+        val drawableResource1 = when (dice1) {
             1 -> R.drawable.dice_1
             2 -> R.drawable.dice_2
             3 -> R.drawable.dice_3
@@ -34,8 +36,45 @@ class MainActivity : AppCompatActivity() {
             5 -> R.drawable.dice_5
             else -> R.drawable.dice_6
         }
-        val diceImage: ImageView = findViewById(R.id.dice_image)
-        diceImage.setImageResource(drawableResource)
+
+        val drawableResource2 = when (dice2) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        val diceImage1: ImageView = findViewById(R.id.dice_image)
+        val diceImage2: ImageView = findViewById(R.id.dice_image_2)
+        diceImage1.setImageResource(drawableResource1)
+        diceImage2.setImageResource(drawableResource2)
+        return dice1 == dice2
 
     }
+
+
+    private fun rollonethousand() {
+        val result: TextView = findViewById(R.id.result)
+        var count = 0
+        val x = 10
+
+
+        for (i in 1..1000){
+
+            if (rollDice()) {
+                count = count + 1
+            }
+        }
+        var percent = count.toDouble() / x.toDouble()
+
+
+        result.text = "Score: ${percent} % "
+
+    }
+
 }
+
+
+
+
